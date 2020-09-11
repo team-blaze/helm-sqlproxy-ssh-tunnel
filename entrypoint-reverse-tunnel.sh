4a6677d067043df0f990df9104d1cfb1ab915301
Daniel Demmel <daniel.demmel@beryl.cc>
2020-09-11 12:19:11 +0100

    ✨ Improve startup script

--- entrypoint-reverse-tunnel.sh
+++ entrypoint-reverse-tunnel.sh
@@ -2,4 +2,7 @@
 
 while [ ! -f /root/reverse_tunnel_ed25519 ]; do echo "Waiting for private key to be present" && sleep 1; done
 
-autossh -M 0 -f -N -R $PORT_BINDING $SSH_LOGIN -g -i /root/reverse_tunnel_ed25519 -o ServerAliveInterval=10 -o ServerAliveCountMax=1 -o ExitOnForwardFailure=yes -o StrictHostKeyChecking=accept-new
+echo "[INFO] Using $(autossh -V)"
+echo "[INFO] Tunneling $SSH_LOGIN with port binding $PORT_BINDING"
+
+exec autossh -M 0 -N -t -R $PORT_BINDING $SSH_LOGIN -g -i /root/reverse_tunnel_ed25519 -o ServerAliveInterval=10 -o ServerAliveCountMax=1 -o ExitOnForwardFailure=yes -o StrictHostKeyChecking=accept-new
